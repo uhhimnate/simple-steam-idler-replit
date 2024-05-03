2bcd8f5340ba0f6185b987d829e253fcdfbb3847
uhhimnate <107016038+uhhimnate@users.noreply.github.com>
2024-05-03 02:00:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570, 359550, 230410, 1245620, 105600];  // Enter here AppIDs of the needed games
+var games = [359550, 230410, 1245620, 105600];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
